1a0815c3f0ad38467adad67ddee896abc9627a3a
Erfan Moharamzadeh <erfan.mzd2000@gmail.com>
2022-06-08 14:10:42 +0430

    init def preceptron class

--- preceptron.py
+++ preceptron.py
@@ -1,2 +1,14 @@
 class Preceptron():
-	pass
+	def __init__(self, alpha):
+		self.step = 0
+		self.alpha = alpha
+		self.y_in = None
+		self.astaneh = None
+
+	def y_function(self):
+		if self.y_in > self.astaneh:
+			return 1
+		elif self.y_in < (-1 * self.astaneh):
+			return -1
+		else:
+			return 0
