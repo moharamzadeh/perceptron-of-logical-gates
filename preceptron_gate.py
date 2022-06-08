a8b76369064cc8addb62e515378e13d82ef5675c
Erfan Moharamzadeh <erfan.mzd2000@gmail.com>
2022-06-08 19:54:55 +0430

    fix and update __init__ preceptron_gate

--- preceptron_gate.py
+++ preceptron_gate.py
@@ -2,11 +2,11 @@ from preceptron import Preceptron
 from gate import *
 
 class Preceptron_Gate(Preceptron):
-	def __init__(self, alpha, datas, targets, weitghts, gate):
-		super().__init__(alpha, datas, targets, weitghts)
+	def __init__(self, gate, alpha, weitghts):
 		self.gate = Gate(type=gate)
-		self.setDatas = self.gate.states
-		self.setTargets = self.gate.target
+		datas = self.gate.states
+		targets = self.gate.target
+		super().__init__(alpha, datas, targets, weitghts)
 
 	def getStateX(self):
 		self.gate.getState(self.step)
