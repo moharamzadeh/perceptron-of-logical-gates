19ecc866ddeec411633a23cfbc039112e5ccd283
Erfan Moharamzadeh <erfan.mzd2000@gmail.com>
2022-06-08 16:53:05 +0430

    y_function use function __y_in

--- preceptron.py
+++ preceptron.py
@@ -15,11 +15,11 @@ class Preceptron():
 		self.bias = value
 
 	def y_function(self):
-		if self.y_in > 0:
+		if self.__y_in_function() > 0:
 			return 1
 		return 0
 
-	def y_in_function(self):
+	def __y_in_function(self):
 		y_in = self.bias + (self.W[0]*self.X[0] + self.W[1]*self.X[1])
 		return y_in
 
