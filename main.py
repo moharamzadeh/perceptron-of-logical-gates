c7e13e818253ef380e39256aa9898886fcf4bae8
Erfan Moharamzadeh <erfan.mzd2000@gmail.com>
2022-06-08 22:19:16 +0430

    write main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from preceptron_gate import *
+
+def main(gate, alpha, w1, w2):
+	W_array = [w1, w2]
+	prec = Preceptron_Gate(gate=gate, alpha=alpha, weitghts=W_array)
+	for i in range(100):
+		break_step = 0
+		if break_step > 4:
+			break
+		shart = prec.nextIteration()
+		if not shart:
+			break_step += 1
+	
+	return prec.W, prec.bias
