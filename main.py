27556aaba23cd4d2ca899ab14d7c4f0cb4a075f1
Erfan Moharamzadeh <erfan.mzd2000@gmail.com>
2022-06-08 23:18:10 +0430

    delete break step

--- main.py
+++ main.py
@@ -4,11 +4,6 @@ def main(gate, alpha, w1, w2):
 	W_array = [w1, w2]
 	prec = Preceptron_Gate(gate=gate, alpha=alpha, weitghts=W_array)
 	for i in range(100):
-		break_step = 0
-		if break_step > 4:
-			break
-		error_nadashtan = prec.nextIteration()
-		if error_nadashtan:
-			break_step += 1
+		prec.nextIteration()
 	
 	return prec.W, prec.bias
