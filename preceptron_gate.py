2b35aac738024bc3c2b5945c9f22e77a09f1393d
Erfan Moharamzadeh <erfan.mzd2000@gmail.com>
2022-06-08 19:43:06 +0430

    change gates

--- preceptron_gate.py
+++ preceptron_gate.py
@@ -2,9 +2,11 @@ from preceptron import Preceptron
 from gate import *
 
 class Preceptron_Gate(Preceptron):
-	def __init__(self, alpha, gate):
-		super().__init__(alpha)
+	def __init__(self, alpha, datas, targets, weitghts, gate):
+		super().__init__(alpha, datas, targets, weitghts)
 		self.gate = Gate(type=gate)
+		self.setDatas = self.gate.states
+		self.setTargets = self.gate.target
 
 	def getStateX(self):
 		self.gate.getState(self.step)
