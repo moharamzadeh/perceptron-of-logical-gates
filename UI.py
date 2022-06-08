84584a28ec6718c1eb8a6e453c3687d22c6aae19
Erfan Moharamzadeh <erfan.mzd2000@gmail.com>
2022-06-08 14:29:19 +0430

    delete lines of setObjectName

--- UI.py
+++ UI.py
@@ -2,7 +2,6 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_window(object):
 	def setupUi(self, window):
-		window.setObjectName("window")
 		window.resize(435, 240)
 		window.setMinimumSize(QtCore.QSize(435, 240))
 		window.setMaximumSize(QtCore.QSize(435, 240))
@@ -13,32 +12,25 @@ class Ui_window(object):
 
 		self.txtX = QtWidgets.QLineEdit(window)
 		self.txtX.setGeometry(QtCore.QRect(30, 10, 31, 33))
-		self.txtX.setObjectName("txtX")
 
 		self.txtY = QtWidgets.QLineEdit(window)
 		self.txtY.setGeometry(QtCore.QRect(100, 10, 31, 33))
-		self.txtY.setObjectName("txtY")
 
 		self.txtAlpha = QtWidgets.QLineEdit(window)
 		self.txtAlpha.setGeometry(QtCore.QRect(100, 70, 41, 33))
-		self.txtAlpha.setObjectName("txtAlpha")
 
 		self.txtW1 = QtWidgets.QLineEdit(window)
 		self.txtW1.setGeometry(QtCore.QRect(40, 160, 41, 33))
-		self.txtW1.setObjectName("txtW1")
 
 		self.txtW2 = QtWidgets.QLineEdit(window)
 		self.txtW2.setGeometry(QtCore.QRect(40, 200, 41, 33))
-		self.txtW2.setObjectName("txtW2")
 
 		self.comboBox = QtWidgets.QComboBox(window)
 		self.comboBox.setGeometry(QtCore.QRect(115, 160, 85, 33))
-		self.comboBox.setObjectName("comboBox")
 		self.comboBox.addItems(['AND', 'OR', 'NOR', 'NAND'])
 
 		self.btnMohasebeh = QtWidgets.QPushButton(window)
 		self.btnMohasebeh.setGeometry(QtCore.QRect(115, 200, 85, 33))
-		self.btnMohasebeh.setObjectName("btnMohasebeh")
 
 		self.txtOutput = QtWidgets.QTextBrowser(window)
 		self.txtOutput.setGeometry(QtCore.QRect(210, 10, 211, 221))
@@ -47,47 +39,37 @@ class Ui_window(object):
 		font.setPointSize(8)
 
 		self.txtOutput.setFont(font)
-		self.txtOutput.setObjectName("txtOutput")
 
 		self.lblX = QtWidgets.QLabel(window)
 		self.lblX.setGeometry(QtCore.QRect(10, 20, 21, 17))
-		self.lblX.setObjectName("lblX")
 
 		self.lblY = QtWidgets.QLabel(window)
 		self.lblY.setGeometry(QtCore.QRect(80, 20, 21, 17))
-		self.lblY.setObjectName("lblY")
 
 		self.lblAlpha = QtWidgets.QLabel(window)
 		self.lblAlpha.setGeometry(QtCore.QRect(10, 80, 71, 17))
-		self.lblAlpha.setObjectName("lblAlpha")
 
 		self.line = QtWidgets.QFrame(window)
 		self.line.setGeometry(QtCore.QRect(10, 50, 151, 16))
 		self.line.setFrameShape(QtWidgets.QFrame.HLine)
 		self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
-		self.line.setObjectName("line")
 
 		self.line2 = QtWidgets.QFrame(window)
 		self.line2.setGeometry(QtCore.QRect(10, 110, 151, 16))
 		self.line2.setFrameShape(QtWidgets.QFrame.HLine)
 		self.line2.setFrameShadow(QtWidgets.QFrame.Sunken)
-		self.line2.setObjectName("line2")
 
 		self.lblW = QtWidgets.QLabel(window)
 		self.lblW.setGeometry(QtCore.QRect(40, 130, 41, 17))
-		self.lblW.setObjectName("lblW")
 
 		self.btnClear = QtWidgets.QPushButton(window)
 		self.btnClear.setGeometry(QtCore.QRect(170, 10, 31, 33))
-		self.btnClear.setObjectName("btnClear")
 
 		self.lblW1 = QtWidgets.QLabel(window)
 		self.lblW1.setGeometry(QtCore.QRect(10, 170, 31, 17))
-		self.lblW1.setObjectName("lblW1")
 
 		self.lblW2 = QtWidgets.QLabel(window)
 		self.lblW2.setGeometry(QtCore.QRect(10, 210, 31, 17))
-		self.lblW2.setObjectName("lblW2")
 
 		self.retranslateUi(window)
 		QtCore.QMetaObject.connectSlotsByName(window)
